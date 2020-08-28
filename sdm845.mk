7e740ec74e929ebac6f0d99190d8334cfce42c9c
Saikiran <bjsaikiran@gmail.com>
2020-08-28 13:07:03 +0530

    sdm845-common: Build ldac libs
    Change-Id: I6c3b6092d0b87a7ae8838c8df564450bb08357fd
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>
    Signed-off-by: Cruzer1 <preetamswain143@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -110,6 +110,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default \
     libbthost_if \
+    libldacBT_enc \
+    libldacBT_abr \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
